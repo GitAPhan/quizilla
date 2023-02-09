9a8c99583a7b425de800c10d6395dd1c031d28de
panda1 <blueblue0011@gmail.com>
2023-02-08 19:35:57 -0800

    added homepage vue but dynamic image has a problem

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <div id='app'>
-    <router-view></router-view>
+    <nav>
+      <h1> Quizzilla app.vue page</h1>
+      <!-- <img :src = "src/assets/godzilla.png"> -->
+    </nav>
+      <router-view/>
+      
+      <!-- <router-link to="/home">testing2</router-link> -->
+    
   </div>
 </template>
 
 <script>
+
+  
+
+
 </script>
 
 <style>
-  .test {
-    font-size: 12px;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Quizzilla</h1>
+        <img src=godzilla alt="some lizard" />
+        <start-game></start-game>
+        <add-question></add-question>
+
+        
+        
+        
+
+    </div>
+</template>
+
+<script>
+import StartGame from '@/components/StartGame.vue'
+import AddQuestion from '@/components/AddQuestion.vue'
+
+    export default {
+        data() {
+            return {
+                godzilla: "https://img.pokemondb.net/sprites/sword-shield/icon/venusaur.png"
+            }
+        },
+  components: { StartGame, AddQuestion },
+        name: 'home-page'
+    }
+</script>
+
+<style scoped>
+
+</style>
