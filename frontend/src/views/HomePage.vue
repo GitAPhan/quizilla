72ecedf0229470b344170b5247d53e4731a4b092
panda1 <blueblue0011@gmail.com>
2023-02-08 20:58:23 -0800

    changed src in homepage.vue into static

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Quizzilla</h1>
-        <img src=godzilla alt="some lizard" />
+        <img src="@/assets/godzilla.png" alt="some lizard" />
         <start-game></start-game>
         <add-question></add-question>
 
@@ -17,16 +17,17 @@ import StartGame from '@/components/StartGame.vue'
 import AddQuestion from '@/components/AddQuestion.vue'
 
     export default {
-        data() {
-            return {
-                godzilla: "https://img.pokemondb.net/sprites/sword-shield/icon/venusaur.png"
-            }
-        },
+        // data() {
+        //     return {
+        //         godzilla: "https://i.imgur.com/mGlDzAY.png"
+        //     }
+        // },
   components: { StartGame, AddQuestion },
         name: 'home-page'
     }
 </script>
 
 <style scoped>
+
 
 </style>
