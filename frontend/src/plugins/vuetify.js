da51e002c71072a3e682341a31bf3fd5fdbdc989
GitAPhan <itsandrewphan@gmail.com>
2023-02-16 00:32:40 -0700

    added vuetify to project and color theem[3~[3~[mes

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -3,5 +3,28 @@ import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-});
+const vuetify = new Vuetify({
+    theme: {
+        dark: true,
+        themes: {
+            dark: {
+                primary: '#00dcc3',
+                text: '#f7f7f7',
+                accent: '#450303',
+                secondary: '#717070',
+                anchor: '#d6cfcf',
+                background: "#1f1f1f"
+            },
+            light: {
+                primary: '#d6cfcf',
+                text: '#1f1f1f',
+                accent: '#450303',
+                secondary: '#a59e9e',
+                anchor: '#464545',
+                background: "#f7f7f7"
+            }
+        },
+    },
+})
+
+export default vuetify
